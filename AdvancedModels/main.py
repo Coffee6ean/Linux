deda3cccc33cd39bb80ae2219c60d4c4577653f5
Coffee6ean <mejiapazalfredo@gmail.com>
2024-02-20 21:27:02 -0600

    Adding a 'Higlighting' step to compensate for image erosion and line deteriorate

--- AdvancedModels/main.py
+++ AdvancedModels/main.py
@@ -1,17 +1,15 @@
 import subprocess
-import sys
-sys.path.append("../")
 
 # Define the paths to your scripts
-script1_path = '/home/coffee_6ean/Linux/AdvancedModels/ImageProcessing/ImageWashing/model.py'
-script2_path = '/home/coffee_6ean/Linux/AdvancedModels/ImageProcessing/LineDetection/model.py'
+washing_script_path = '/home/coffee_6ean/Linux/AdvancedModels/ImageProcessing/ImageWashing/model.py'
+highlighting_script_path = '/home/coffee_6ean/Linux/AdvancedModels/ImageProcessing/ImageHighlight/model.py'
+line_detection_script_path = '/home/coffee_6ean/Linux/AdvancedModels/ImageProcessing/LineDetection/model.py'
 
-# Run the first script
-subprocess.run(['python', script1_path])
+# Run the image washing script
+subprocess.run(['python3', washing_script_path])
 
-# Once the first script finishes, move the resulting image to a folder
-# This assumes that the script1.py script saves the washed image as 'washed_image.jpg'
-subprocess.run(['mv', 'washed_image.jpg', 'path/to/your/output/folder/'])
+# Run the image highlighting script
+subprocess.run(['python3', highlighting_script_path])
 
-# Run the second script, which performs line detection on the washed image
-subprocess.run(['python', script2_path])
+# Run the line detection script
+subprocess.run(['python3', line_detection_script_path])
