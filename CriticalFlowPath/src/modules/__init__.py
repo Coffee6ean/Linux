366c24641468f17b39ba6c521118de252bd01bc7
Coffee6ean <mejiapazalfredo@gmail.com>
2024-08-15 19:21:28 -0600

    Added '__init__.py' to 'src/modules/': Allows for directories to be imported as libraries

---
+++ CriticalFlowPath/src/modules/__init__.py
@@ -0,0 +1,4 @@
+from modules.pdf_to_jason import PdfToJson  
+from modules.json_to_excel import JsonToExcel  
+from modules.excel_post_processing import ExcelPostProcessing  
+from modules.schedule_framework import ScheduleFramework  
