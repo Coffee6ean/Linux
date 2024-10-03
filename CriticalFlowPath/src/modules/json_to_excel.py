41207e73f60b7955dee955f902e53cec8a9da2ae
Coffee6ean <mejiapazalfredo@gmail.com>
2024-10-03 12:59:11 -0600

    Modified 'json_to_excel' file. Mods include: 1. Modified file to adhere to coding standards 2. Still to polish and refine -> WIP

--- CriticalFlowPath/src/modules/json_to_excel.py
+++ CriticalFlowPath/src/modules/json_to_excel.py
@@ -40,7 +40,7 @@ class JsonToExcel:
                 return -1, None  
 
             base_name = dir_list[selection]
-            print(f'File selected: {base_name}')
+            print(f"File selected: {base_name}")
             file = os.path.join(path, base_name)
 
             if JsonToExcel.is_json(file):
@@ -55,32 +55,32 @@ class JsonToExcel:
             else:
                 return -1, None
         else: 
-            print('Error: Please verify the directory and file exist and that the file is of type .json')    
+            print("Error: Please verify the directory and file exist and that the file is of type .json")    
             return -1, None
 
     @staticmethod
     def display_directory_files(list):
         if len(list) == 0:
-            print('Error: No files found')
+            print("Error: No files found")
             return -1
 
         if len(list) > 1:
-            print(f'-- {len(list)} files found:')
+            print(f"-- {len(list)} files found:")
             for idx, file in enumerate(list, start=1):
-                print(f'{idx}. {file}')
-            selection_idx = input('\nPlease enter the index number to select the one to process: ')
+                print(f"{idx}. {file}")
+            selection_idx = input("\nPlease enter the index number to select the one to process: ")
         else:
-            print(f'Single file found: {list[0]}')
+            print(f"Single file found: {list[0]}")
             return 0  
 
         return int(selection_idx) - 1
 
     @staticmethod
     def is_json(file_name):
-        if file_name.endswith('.json'):
+        if file_name.endswith(".json"):
             return True
         else:
-            print('Error: Selected file is not a JSON file')
+            print("Error: Selected file is not a JSON file")
             return False
 
     def write_data_to_excel(self):
@@ -95,45 +95,45 @@ class JsonToExcel:
             return
 
         if data:
-            with pd.ExcelWriter(excel_file, engine='openpyxl', mode='w') as writer:
+            with pd.ExcelWriter(excel_file, engine="openpyxl", mode='w') as writer:
                 
-                metadata = data.get('project_metadata', {})
-                metadata_df = pd.DataFrame(metadata.items(), columns=['Field', 'Value'])
-                metadata_df.to_excel(writer, sheet_name='Metadata', index=False, startrow=1)
-                body = data.get('project_content', {}).get('body', [])
+                metadata = data.get("project_metadata", {})
+                metadata_df = pd.DataFrame(metadata.items(), columns=["Field", "Value"])
+                metadata_df.to_excel(writer, sheet_name="Metadata", index=False, startrow=1)
+                body = data.get("project_content", {}).get("body", [])
                 flattened_data = []
 
                 for parent_data in body:
                     parent_obj_frame = {
                         "parent_id": "N/A",  
-                        'id': parent_data.get('id', ''),
-                        'name': parent_data.get('name', ''),
-                        'duration': parent_data.get('duration', ''),
-                        'start': parent_data.get('start', ''),
-                        'finish': parent_data.get('finish', ''),
-                        'total_float': parent_data.get('total_float', ''),
-                        "scope_of_work": parent_data.get('scope_of_work', ''),
-                        "phase": parent_data.get('phase', ''),
-                        "trade": parent_data.get('trade', ''),
-                        "company": parent_data.get('company', ''),
-                        "location": parent_data.get('location', ''),
+                        "id": parent_data.get("id", ""),
+                        "name": parent_data.get("name", ""),
+                        "duration": parent_data.get("duration", ""),
+                        "start": parent_data.get("start", ""),
+                        "finish": parent_data.get("finish", ""),
+                        "total_float": parent_data.get("total_float", ""),
+                        "scope_of_work": parent_data.get("scope_of_work", ""),
+                        "phase": parent_data.get("phase", ""),
+                        "trade": parent_data.get("trade", ""),
+                        "company": parent_data.get("company", ""),
+                        "location": parent_data.get("location", ""),
                     }
                     flattened_data.append(parent_obj_frame)
 
-                    for child_data in parent_data.get('activities', []):
+                    for child_data in parent_data.get("activities", []):
                         child_obj_frame = {
-                            "parent_id": parent_data.get('id', ''),
-                            'id': child_data.get('id', ''),
-                            'name': child_data.get('name', ''),
-                            'duration': child_data.get('duration', ''),
-                            'start': child_data.get('start', ''),
-                            'finish': child_data.get('finish', ''),
-                            'total_float': child_data.get('total_float', ''),
-                            "scope_of_work": child_data.get('scope_of_work', ''),
-                            "phase": child_data.get('phase', ''),
-                            "trade": child_data.get('trade', ''),
-                            "company": child_data.get('company', ''),
-                            "location": child_data.get('location', ''),
+                            "parent_id": parent_data.get("id", ""),
+                            "id": child_data.get("id", ""),
+                            "name": child_data.get("name", ""),
+                            "duration": child_data.get("duration", ""),
+                            "start": child_data.get("start", ""),
+                            "finish": child_data.get("finish", ""),
+                            "total_float": child_data.get("total_float", ""),
+                            "scope_of_work": child_data.get("scope_of_work", ""),
+                            "phase": child_data.get("phase", ""),
+                            "trade": child_data.get("trade", ""),
+                            "company": child_data.get("company", ""),
+                            "location": child_data.get("location", ""),
                         }
                         flattened_data.append(child_obj_frame)
 
@@ -143,6 +143,7 @@ class JsonToExcel:
             print(f"Successfully converted JSON to Excel and saved to {excel_file}")
         else:
             print("Error: No data to convert.")
+
 
 if __name__ == "__main__":
     JsonToExcel.main()
