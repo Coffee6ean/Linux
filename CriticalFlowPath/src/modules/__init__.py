6cda1f6e3da62a973c17d6fc4199f409e3cdff91
Coffee6ean <mejiapazalfredo@gmail.com>
2024-10-03 13:13:14 -0600

    Modified '__init__' file. Changed file import address

--- CriticalFlowPath/src/modules/__init__.py
+++ CriticalFlowPath/src/modules/__init__.py
@@ -1,4 +1,4 @@
-from modules.pdf_to_jason import PdfToJson  
+from CriticalFlowPath.src.modules.pdf_to_json import PdfToJson  
 from modules.json_to_excel import JsonToExcel  
 from modules.excel_post_processing import ExcelPostProcessing  
 from modules.schedule_framework import ScheduleFramework  
