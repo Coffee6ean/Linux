09dd04e7f168e25fe159394b5c40f5a8ac9b7425
Coffee6ean <mejiapazalfredo@gmail.com>
2025-01-23 19:27:18 -0600

    Refactored package file 'utils/__init__'. Mods include: 1. Improved imports: Added 'DataIngestion' import from the 'data_ingestion' module and updated relative imports for better package structure. 2. Cleaned up code: Removed commented-out imports for cleaner and more maintainable code. 3. Still to polish and refine -> WIP.

--- CriticalFlowPath/src/modules/utils/__init__.py
+++ CriticalFlowPath/src/modules/utils/__init__.py
@@ -1,5 +1,7 @@
 # Imported Helper - As Package
+from .data_ingestion import DataIngestion
 from .data_frame_setup import DataFrameSetup
 
 # Imported Helper - As Module 
-#from utils.data_frame_setup import DataFrameSetup
+""" from utils.data_ingestion import DataIngestion
+from utils.data_frame_setup import DataFrameSetup """
