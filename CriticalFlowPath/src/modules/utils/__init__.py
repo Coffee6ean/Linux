5ab67ee4a7772c81d8e3cc04041f096e5028161c
Coffee6ean <mejiapazalfredo@gmail.com>
2025-01-24 19:48:34 -0600

    Updated imports in helper file 'utils/__init__'. Mods include: 1. Added 'DataRelationship' import to support hyperlink creation and data relationship functionality. 2. Updated commented-out module imports to include 'DataRelationship' for consistency. 3. Still to polish and refine -> WIP.

--- CriticalFlowPath/src/modules/utils/__init__.py
+++ CriticalFlowPath/src/modules/utils/__init__.py
@@ -1,7 +1,9 @@
 # Imported Helper - As Package
 from .data_ingestion import DataIngestion
 from .data_frame_setup import DataFrameSetup
+from .data_relationship import DataRelationship
 
 # Imported Helper - As Module 
 """ from utils.data_ingestion import DataIngestion
-from utils.data_frame_setup import DataFrameSetup """
+from utils.data_frame_setup import DataFrameSetup
+from utils.data_relationship import DataRelationship """
