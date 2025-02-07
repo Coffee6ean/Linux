90c54f7c99fb73ec675ed3f1ffb1928a23093def
Coffee6ean <mejiapazalfredo@gmail.com>
2025-02-07 17:43:51 -0600

    Refactored imports in 'modules/__init__' file. Mods include: 1. Fixed Syntax: Cleaned up import statements 2. Improved Structure: Reorganized imports to prioritize local package imports (using from .) over absolute module imports for better modularity and maintainability. 3. Enhanced Readability: Commented out the absolute module imports to provide clarity on alternative import methods while keeping the local package imports active. 4. Streamlined Dependencies: Ensured all necessary frameworks (Setup, WbsFramework, ScheduleFramework, LegendsFramework, PostProcessingFramework, AnalyticsFramework) are imported consistently. 5. Still to polish and refine -> WIP.

--- CriticalFlowPath/src/modules/__init__.py
+++ CriticalFlowPath/src/modules/__init__.py
@@ -1,6 +1,15 @@
-from modules.setup import Setup
+# Imported Helper - As Package 
+from .setup import Setup
+from .wbs_framework import WbsFramework
+from .schedule_framework import ScheduleFramework
+from .legends_framework import LegendsFramework
+from .post_processing_framework import PostProcessingFramework  
+from .analytics_framework import AnalyticsFramework
+
+# Imported Helper - As Module
+""" from modules.setup import Setup
 from modules.wbs_framework import WbsFramework
 from modules.schedule_framework import ScheduleFramework
 from modules.legends_framework import LegendsFramework
 from modules.post_processing_framework import PostProcessingFramework  
-from modules.analytics_framework import AnalyticsFramework
+from modules.analytics_framework import AnalyticsFramework """
