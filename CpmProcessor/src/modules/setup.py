14c0974f266d64cb7cc3a114cf7b60e87ee648f6
Coffee6ean <mejiapazalfredo@gmail.com>
2025-02-25 11:00:49 -0600

    Added new 'setup' file. Mods include:
    1. Purpose: Centralizes project setup and initialization, handling data ingestion and referencing workflows.
    2. Functionality: Supports both automated (auto=True) and manual (auto=False) initialization via generate_ins.
    3. Functionality: Processes data extraction and referencing using _extract_data_from_file and _reference_data_from_file.
    4. Updates project modules with extracted and referenced data for downstream use.
    5. Functionality: Logs progress and results for better tracking and debugging.
    6. Work in Progress: Currently under development, with ongoing refinements to improve data handling and module integration.

---
+++ CpmProcessor/src/modules/setup.py
@@ -0,0 +1,229 @@
+import os
+import re
+import json
+from datetime import datetime
+
+# Imported Helper - As Package 
+from modules.utils.xlsx.data_ingestion import XlsxDataIngestion
+from modules.utils.xlsx.data_referencing import XlsxDataReferencing
+
+# Imported Helper - As Module
+""" from utils.xlsx.data_ingestion import XlsxDataIngestion
+from utils.xlsx.data_referencing import XlsxDataReferencing """
+
+import sys
+sys.path.append("../")
+from CpmProcessor.keys.secrets import RSLTS_DIR, TEST_PDF_DIR, TEST_XLSX_DIR
+
+class Setup:
+    modules = 0
+
+    def __init__(self, project_dict) -> None:
+        self.obj = project_dict
+
+    @staticmethod
+    def main(auto=True):
+        ins = Setup.generate_ins()
+
+        ins._print_result("DataIngestion processing...")
+        data = ins._extract_data_from_file(
+            auto, 
+            ins.obj["input_file"]["path"], 
+            ins.obj["input_file"]["basename"], 
+            ins.obj["input_file"]["extension"],
+            RSLTS_DIR
+        )
+        ins._update_project_modules(data)
+
+        ins._print_result("DataReferencing processing...")
+        reference = ins._reference_data_from_file(
+            auto, 
+            ins.obj["input_file"]["path"], 
+            ins.obj["input_file"]["basename"], 
+            ins.obj["input_file"]["extension"],
+            data["content"]["body"]
+        )
+        ins._update_project_modules(reference)
+
+
+        return ins
+
+    @staticmethod
+    def generate_ins():
+        input_file = Setup.return_valid_file(
+            input("Please enter the path to the file or directory: ").strip()
+        )
+        input_project_client = input("Enter Project Client: ").strip()
+        input_project_name = input("Enter Project Name: ").strip()
+        input_project_code = input("Enter Project Code: ").strip()
+        input_project_title = input("Enter Project Title: ").strip()
+        input_project_subtitle = input("Enter Project Subtitle: ").strip()
+        input_project_workweek = input("Enter Project Workweek (default. Mon-Sun): ").strip()
+        input_project_location = input("Enter Project Location: ").strip()
+        input_project_asignee = input("Enter Project Assignee: ").strip()
+        input_project_tags = input("Enter Project Tags: ").strip()
+
+        project_ins_dict = {
+            "input_file": dict(
+                path = input_file.get("path"),
+                basename = input_file.get("basename"),
+                extension = input_file.get("extension"),
+            ),
+            "output_file": dict(
+                parent_directory = "== SECRET ==",
+                directories = {},
+            ),
+            "project": dict(
+                metadata = dict(
+                    id = None,
+                    continuity = None,
+                    dates = dict(
+                        created = Setup.return_valid_date(),
+                        finished = None,
+                    ),
+                    client = input_project_client,
+                    name = input_project_name,
+                    code = input_project_code,
+                    title = input_project_title,
+                    subtitle = input_project_subtitle,
+                    workweek = input_project_workweek if input_project_workweek else "Mon-Sun",
+                    location = input_project_location,
+                    assignee = input_project_asignee,
+                    tags = input_project_tags,
+                ),
+                modules = {},
+            ),
+        }
+
+        ins = Setup(project_ins_dict)
+
+        return ins
+    
+    @staticmethod
+    def return_valid_file(input_file_dir:str) -> dict|int:        
+        if not os.path.exists(input_file_dir):
+            raise FileNotFoundError("Error: Given directory or file does not exist in the system.")
+
+        if os.path.isdir(input_file_dir):
+            file_dict = Setup._handle_dir(input_file_dir)
+        elif os.path.isfile(input_file_dir):
+            file_dict = Setup._handle_file(input_file_dir)
+
+        return file_dict
+
+    @staticmethod
+    def _handle_dir(input_file_dir:str, mode:str="r") -> dict|int:
+        if mode in ['u', 'r', 'd']:
+            dir_list = os.listdir(input_file_dir)
+            selection = DataIngestion._display_directory_files(dir_list)
+            input_file_basename = dir_list[selection]
+            print(f'File selected: {input_file_basename}\n')
+        elif mode == 'c':
+            input_file_basename = None
+        else:
+            print("Error: Invalid mode specified.")
+            return -1
+        
+        return dict(
+            path = os.path.dirname(input_file_dir), 
+            basename = os.path.basename(input_file_basename).split(".")[0],
+            extension = os.path.basename(input_file_basename).split(".")[-1],
+        )
+    
+    @staticmethod
+    def _display_directory_files(file_list:list) -> int:
+        selection_idx = -1  
+
+        if len(file_list) == 0:
+            print('Error. No files found')
+            return -1
+        
+        print(f'-- {len(file_list)} files found:')
+        for idx, file in enumerate(file_list, start=1):  
+            print(f'{idx}. {file}')
+
+        while True:
+            try:
+                selection_idx = int(input('\nPlease enter the index number to select the one to process: '))
+                if 1 <= selection_idx <= len(file_list):  
+                    return selection_idx - 1  
+                else:
+                    print(f'Error: Please enter a number between 1 and {len(file_list)}.')
+            except ValueError:
+                print('Error: Invalid input. Please enter a valid number.\n')
+
+    @staticmethod
+    def _handle_file(input_file_dir:str):
+        input_file_extension = os.path.basename(input_file_dir).split(".")[-1]
+
+        if (input_file_extension in ["xlsx", "xml"]):
+            return dict(
+                path = os.path.dirname(input_file_dir), 
+                basename = os.path.basename(input_file_dir).split(".")[0],
+                extension = input_file_extension,
+            )
+
+        print("Error: Please verify that the directory and file exist and that the file extension complies with class attributes")
+        return -1
+
+    @staticmethod
+    def return_valid_date() -> str:
+        now = datetime.now()
+        date_str = now.strftime("%d-%b-%y %H:%M:%S")
+
+        return date_str
+
+    @staticmethod
+    def return_valid_module_key() -> str:
+        count = Setup.modules + 1
+        Setup.modules = count
+
+        return f"MODULE_{count}"
+
+    @staticmethod
+    def write_data_to_json(file_title:str, json_dict:dict):
+        file = os.path.join(RSLTS_DIR, file_title)
+        
+        with open(file, 'w') as writer:
+            json.dump(json_dict, writer)
+
+    def _extract_data_from_file(self, auto:str, input_file_path=None, input_file_basename=None, 
+                                input_file_extension=None, output_file_dir=None) -> dict:
+        data = XlsxDataIngestion.main(
+            auto,
+            input_file_path,
+            input_file_basename,
+            input_file_extension,
+            output_file_dir
+        )   
+
+        return data
+
+    def _reference_data_from_file(self, auto:str, input_file_path=None, input_file_basename=None, 
+                                  input_file_extension=None, project_data_dict=None) -> dict:
+        reference = XlsxDataReferencing.main(
+            auto,
+            input_file_path,
+            input_file_basename,
+            input_file_extension,
+            project_data_dict
+        )
+
+        return reference
+
+    def _update_project_modules(self, data:dict) -> None:
+        module_key = self.return_valid_module_key()
+        if module_key:
+            self.obj["project"]["modules"][module_key] = data
+        else:
+            raise ValueError("Invalid module key")
+
+    def _print_result(self, prompt_message:str) -> None:
+        print()
+        print()
+        print(f'//========== {prompt_message} ==========//')
+        print()
+
+
+if __name__ == "__main__":
+    setup = Setup.main()
