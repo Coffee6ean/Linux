548eb599bcaceeaee33f9c632c8baf72ecec13ef
Coffee6ean <mejiapazalfredo@gmail.com>
2025-02-26 21:50:29 -0600

    Added 'CpmProcessor/src/modules/__init__' file. Features include: 1. Purpose: Enables modular imports and streamlined usage of all submodules within the modules directory. 2. Functionality: Exposes key submodules (e.g., setup, utils) for external use. 3. Functionality: Simplifies imports in other modules by allowing from modules import setup, utils. 4. Work in Progress: Currently under development, with ongoing refinements to improve package structure and functionality.

---
+++ CpmProcessor/src/modules/__init__.py
@@ -0,0 +1,9 @@
+# Imported Helper - As Package 
+from .setup import Setup
+from .table_framework import TableFramework
+from .wbs_framework import WbsFramework
+
+# Imported Helper - As Module
+""" from modules.setup import Setup
+from modules.table_framework import TableFramework
+from modules.wbs_framework import WbsFramework """
