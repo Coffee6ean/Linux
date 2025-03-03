543f8864fbb81a69226c33676bc23571b0022a0c
Coffee6ean <mejiapazalfredo@gmail.com>
2025-03-03 14:35:27 -0600

    Refactored 'utils/__init__' file. Mods include: 1. Expanded Imports: Added XlsxDataReferencing and XlsxDataComparing to support referencing and data comparison operations. 2. Enhanced Functionality: These additions likely enable new features related to data comparison and referencing within the existing framework. 3. Maintained Code Structure: Ensured that the integration of new imports aligns with existing modules.

--- CpmProcessor/src/modules/utils/__init__.py
+++ CpmProcessor/src/modules/utils/__init__.py
@@ -1,3 +1,5 @@
 from .xlsx.data_ingestion import XlsxDataIngestion
+from .xlsx.data_referencing import XlsxDataReferencing
+from .xlsx.data_comparing import XlsxDataComparing
 from .pdf.data_ingestion import PdfDataIngestion
 from .pdf.data_processing import PdfDataProcessing
