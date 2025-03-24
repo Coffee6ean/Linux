249437bece01c0c6f397a13c3ad94fd5285f3250
Coffee6ean <mejiapazalfredo@gmail.com>
2025-03-24 10:20:16 -0600

    Removed files

--- CpmProcessor/src/db/tags/db.sql
+++
@@ -1,9 +0,0 @@
--- Create a 'Tags' polymorphic table
-CREATE TABLE cpm_schema.tags (
-    id SERIAL PRIMARY KEY,
-    entity_type VARCHAR(50) NOT NULL,
-    entity_id INTEGER NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
-);
