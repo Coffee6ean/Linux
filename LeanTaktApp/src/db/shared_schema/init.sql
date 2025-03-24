f4a5b3e8a7d8a3f637739cc5af7449b00b8c1eea
Coffee6ean <mejiapazalfredo@gmail.com>
2025-03-24 14:55:11 -0600

    Added system init script for 'shared_schema': 1. Implements centralized seed script 2. Orchestrates sub-schema seeding 3. Ensures database connection 4. Documents execution order 5. Structures for easy expansion 6. Prepares for automated deployment 7. Plans production validation

---
+++ LeanTaktApp/src/db/shared_schema/init.sql
@@ -0,0 +1,5 @@
+\c lean_takt_db;
+
+-- Call other scripts to generate data pool for 'shared_schema'
+\i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/versions/seed.sql;
+\i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/labels/seed.sql;
