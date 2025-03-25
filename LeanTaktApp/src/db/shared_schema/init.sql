53ca030fa95765e0fdace483f387641366de2a17
Coffee6ean <mejiapazalfredo@gmail.com>
2025-03-25 16:12:15 -0600

    Refactored'shared_schema/init.sql'  initialization file: 1. Purpose: - Streamline database initialization process - Separate table creation from data seeding - Support TypeORM integration 2. Key Changes: - Removed hardcoded connection command (\c lean_takt_db) - Organized script into clear sections: Table creation (db.sql files) & Data seeding (seed.sql files) 3. Impact: - More flexible initialization (works with any DB connection) - Clear separation of schema vs data operations - Better support for automated deployments

--- LeanTaktApp/src/db/shared_schema/init.sql
+++ LeanTaktApp/src/db/shared_schema/init.sql
@@ -1,5 +1,9 @@
-\c lean_takt_db;
+-- Call other scripts to generate data tables (Type ORM)
+\i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/versions/db.sql;
+\i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/labels/db.sql;
+\i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/colors/db.sql;
+\i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/locations/db.sql;
 
--- Call other scripts to generate data pool for 'shared_schema'
+-- Call other scripts to generate data pool for 'auth_schema'
 \i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/versions/seed.sql;
 \i /home/coffee_6ean/Linux/LeanTaktApp/src/db/shared_schema/labels/seed.sql;
