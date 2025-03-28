7a9e4642553d250ad6b449e3b5a627c133c5ffc4
Coffee6ean <mejiapazalfredo@gmail.com>
2025-03-28 14:57:05 -0600

    Added 'auth_schema/users' system. Features: 1. Core Structure: Created users table with comprehensive profile fields 2. Hierarchy Support: Added manager reference for organizational structure 3. Security Foundation: Enforced required role assignment and unique email 4. Profile Flexibility: Included optional fields (picture, about, profession) 5. Pending Items: Password/auth system integration -> WIP

---
+++ LeanTaktApp/src/db/auth_schema/users/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO auth_schema.users 
+(version_id, status_id, role_id, name, middle_name, last_name, birth_date, email, profession)
+VALUES
+(1, 1, 1, 'Alfredo', 'Jafet', 'Mejia Paz', '1998-12-09', 'alfredom@elevateconstructionist.com', 'Software Engineer');
