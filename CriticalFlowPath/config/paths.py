d567ce45287c91a60c0cc83635e2e294c5a490df
Coffee6ean <mejiapazalfredo@gmail.com>
2025-06-06 13:49:27 -0600

    Addd central paths config module at 'config/paths.py'. Mods include: 1. Created 'paths.py' under '/config/' to centralize project directory paths 2. Declared RSLTS_DIR and other shared path constants for consistent imports 3. Updated existing modules to import paths from 'CriticalFlowPath.config.paths'

---
+++ CriticalFlowPath/config/paths.py
@@ -0,0 +1,6 @@
+# Result Directories
+RSLTS_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Results_Local"
+
+# Local Directories
+TEST_XLSX_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/excel"
+TEST_PDF_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/pdf"
