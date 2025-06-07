d64924406f8f138b2d9472094904798f05936c72
Coffee6ean <mejiapazalfredo@gmail.com>
2025-06-06 18:23:13 -0600

    Modified config/paths.py, to include no directory path. Mods include: 1. Declared 'UPLD_FOLDER' for managing uploaded XLSX files 2. Centralized upload directory path in 'paths.py' for consistency across services

--- CriticalFlowPath/config/paths.py
+++ CriticalFlowPath/config/paths.py
@@ -1,6 +1,9 @@
 # Result Directories
 RSLTS_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Results_Local"
 
+#Reciever Directories
+UPLD_FOLDER = '/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Inbox'
+
 # Local Directories
 TEST_XLSX_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/excel"
 TEST_PDF_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/pdf"
