6c0f29741540c2957a6fca6e45c70b47ef962dc3
Coffee6ean <mejiapazalfredo@gmail.com>
2025-06-11 16:36:01 -0600

    Modified 'config/paths.py', updated paths in config. Mods include: 1. Changed 'UPLD_FOLDER' to use 'CFA_Inbox_Local' for local development consistency 2. Removed unused 'EC2_INBOX_DIR' to simplify environment configuration

--- CriticalFlowPath/config/paths.py
+++ CriticalFlowPath/config/paths.py
@@ -2,11 +2,8 @@
 RSLTS_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Results_Local"
 
 #Reciever Directories
-UPLD_FOLDER = '/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Inbox'
+UPLD_FOLDER = "/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Inbox_Local"
 
 # Local Directories
 TEST_XLSX_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/excel"
 TEST_PDF_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/pdf"
-
-#EC2 Directories
-EC2_INBOX_DIR = "/home/ubuntu/CriticalFlowPath/CFA_Inbox"
