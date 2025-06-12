e6b635ce921bf75e2796718cf3aacfab362571d9
Coffee6ean <mejiapazalfredo@gmail.com>
2025-06-11 18:38:29 -0600

    Move all existing backend code and resources into a dedicated 'backend' folder: 1. Organize project structure by nesting current backend files and directories under 'backend' 2. Prepare for clearer separation between backend and other components (e.g., frontend, docs) 3. No changes to code logic, only file system reorganization

---
+++ CriticalFlowPath/backend/config/paths.py
@@ -0,0 +1,9 @@
+# Result Directories
+RSLTS_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/CFA_Results_Local"
+
+#Reciever Directories
+UPLD_FOLDER = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/CFA_Inbox_Local"
+
+# Local Directories
+TEST_XLSX_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/src/modules/local_results/excel"
+TEST_PDF_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/src/modules/local_results/pdf"
--- CriticalFlowPath/config/paths.py
+++
@@ -1,9 +0,0 @@
-# Result Directories
-RSLTS_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Results_Local"
-
-#Reciever Directories
-UPLD_FOLDER = "/home/coffee_6ean/Linux/CriticalFlowPath/CFA_Inbox_Local"
-
-# Local Directories
-TEST_XLSX_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/excel"
-TEST_PDF_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/src/modules/local_results/pdf"
