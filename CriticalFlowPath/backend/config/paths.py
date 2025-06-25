5b4e4e5a8e0f9f10ffc902e25f3f3600a03a25e5
Coffee6ean <mejiapazalfredo@gmail.com>
2025-06-25 12:35:19 -0600

    Modified 'backend/config/paths.py', refactored path constants for test directory into single TST_DIR reference

--- CriticalFlowPath/backend/config/paths.py
+++ CriticalFlowPath/backend/config/paths.py
@@ -1,9 +1,8 @@
-# Result Directories
+# Result Directory
 RSLTS_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/CFA_Results_Local"
 
-#Reciever Directories
+#Reciever Directory
 UPLD_FOLDER = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/CFA_Inbox_Local"
 
-# Local Directories
-TEST_XLSX_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/CFA_Test_Local"
-TEST_PDF_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/CFA_Test_Local"
+# Test Directory
+TST_DIR = "/home/coffee_6ean/Linux/CriticalFlowPath/backend/CFA_Test_Local"
